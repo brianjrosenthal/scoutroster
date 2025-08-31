af90ae1fe92feff5117de562ff35924fa14aae58
brianjrosenthal <brian.rosenthal@gmail.com>
2025-08-30 23:50:31 -0400

    menu change

--- partials.php
+++ partials.php
@@ -22,13 +22,10 @@ function header_html(string $title) {
     $nav .= $link('/index.php','Home').' | ';
     $nav .= $link('/youth.php','Youth').' | ';
     $nav .= $link('/adults.php','Adults').' | ';
-    $nav .= $link('/events.php','Events').' | ';
     $nav .= $link('/my_profile.php','My Profile').' | ';
     if (!empty($u['is_admin'])) {
       $nav .= $link('/admin_adults.php','Manage Adults').' | ';
       $nav .= $link('/admin_import_upload.php','Import Members').' | ';
-      $nav .= $link('/admin_dens.php','Dens').' | ';
-      $nav .= $link('/admin_events.php','Manage Events').' | ';
       $nav .= $link('/admin_mailing_list.php','Mailing List').' | ';
       $nav .= $link('/admin_settings.php','Settings').' | ';
     }
