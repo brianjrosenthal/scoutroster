aa82ec34d29212e23a7fac3b932e77663fd9be64
brianjrosenthal <brian.rosenthal@gmail.com>
2025-09-10 23:35:11 -0400

    forms page change

--- forms.php
+++ forms.php
@@ -7,42 +7,27 @@ header_html('Forms and Links');
 <h2>Forms and Links</h2>
 
 <div class="card">
-  <h3>Medical Form</h3>
-  <p>
-    <a class="button" target="_blank" rel="noopener" href="https://filestore.scouting.org/filestore/HealthSafety/pdf/680-001_AB.pdf">Open Medical Form A&B</a>
-  </p>
+  <h3><a target="_blank" rel="noopener" href="https://filestore.scouting.org/filestore/HealthSafety/pdf/680-001_AB.pdf">Medical Form</a></h3>
   <p class="small">The standard Medical Form A&B required for basic Scouting activities such as local tours and weekend camping trips less than 72 hours in duration.</p>
 </div>
 
 <div class="card">
-  <h3>Youth Application Form</h3>
-  <p>
-    <a class="button" target="_blank" rel="noopener" href="https://filestore.scouting.org/filestore/pdf/524-406.pdf">Open Youth Application</a>
-  </p>
+  <h3><a target="_blank" rel="noopener" href="https://filestore.scouting.org/filestore/pdf/524-406.pdf">Youth Application Form</a></h3>
   <p class="small">Required to register for Cub Scouts.</p>
 </div>
 
 <div class="card">
-  <h3>Adult Application Form</h3>
-  <p>
-    <a class="button" target="_blank" rel="noopener" href="https://filestore.scouting.org/filestore/pdf/524-501.pdf">Open Adult Application</a>
-  </p>
+  <h3><a target="_blank" rel="noopener" href="https://filestore.scouting.org/filestore/pdf/524-501.pdf">Adult Application Form</a></h3>
   <p class="small">Required to become a registered Adult Leader in Cub Scouts.</p>
 </div>
 
 <div class="card">
-  <h3>Greater Hudson Valley Training Dates</h3>
-  <p>
-    <a class="button" target="_blank" rel="noopener" href="https://www.ghvscouting.org/training/">Open Training Dates</a>
-  </p>
+  <h3><a target="_blank" rel="noopener" href="https://www.ghvscouting.org/training/">Greater Hudson Valley Training Dates</a></h3>
   <p class="small">Training dates for den leader training, BALOO training, Wood Badge Training, Rangemaster Training, etc. Note that the page can take a long time to load.</p>
 </div>
 
 <div class="card">
-  <h3>Pack 440 Public Web Site</h3>
-  <p>
-    <a class="button" target="_blank" rel="noopener" href="https://www.scarsdalepack440.com">Visit Pack 440 Website</a>
-  </p>
+  <h3><a target="_blank" rel="noopener" href="https://www.scarsdalepack440.com">Pack 440 Public Web Site</a></h3>
   <p class="small">Our public web site.</p>
 </div>
 
